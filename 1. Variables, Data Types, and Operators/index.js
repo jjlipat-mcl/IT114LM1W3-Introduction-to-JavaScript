feab8cb7e60891aac0a46c1bf580ad3afbaed370
jjlipat-mcl <154210736+jjlipat-mcl@users.noreply.github.com>
2024-01-12 17:25:10 +0000

    finished first exercise

---
+++ 1. Variables, Data Types, and Operators/index.js
@@ -0,0 +1,42 @@
+// TODO 1.1 Use the var keyword to define a variable. Log the variable to the console. Now try moving the variable declaration after you log the variable.
+
+
+// TODO 1.2 Use the let keyword to define a variable.
+
+
+// TODO 1.3 Use the const keyword to define a variable. Try reassigning the value of the variable. What happens?
+
+
+// Checkpoint 1.1 What is the difference between var, let, and const?
+// Answer: 
+
+
+// TODO 1.4 Try out the different arithmetic operators
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#arithmetic_operators
+
+
+// Checkpoint 1.2 What operators did you use?
+// Answer: 
+
+
+// TODO 1.5 Try out the different string operators
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#string_operators
+
+
+// Checkpoint 1.3 What operators did you use?
+
+
+// TODO 1.6 Try out the different logical operators
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#logical_operators
+
+
+// Checkpoint 1.4 What operators did you use?
+// Answer: 
+
+
+// TODO 1.7 Try out the different relational operators
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#relational_operators
+
+
+// Checkpoint 1.5 What does [] == false evaluate to? Why does that happen? Research why
+// Answer: 
