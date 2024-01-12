9d0c3cfe077fb91fed8211a7cf69fe2d246d45e8
jjlipat-mcl <154210736+jjlipat-mcl@users.noreply.github.com>
2024-01-12 22:10:46 +0000

    added checkpoints to exercise 3 and 4

--- 3. Arrays and Objects/index.js
+++ 3. Arrays and Objects/index.js
@@ -1,8 +1,17 @@
-// Todo 3.1 Declare an array of numbers
+let numbers = [
+    1, 89, 55, 63, 29, 19,
+    15, 77, 62, 68, 29, 84,
+    21, 26, 49
+];
+
+// Todo 3.1 Get the first, 5th, and last items in the array.
 // Your code here
 
-// Todo 3.2 Get the first and last items in the array. calculate the average
+// Todo 3.2 calculate the min, max, and the average of the array
 // Your code here
+
+// Checkpoint 3.1 How would you compare lists in python vs lists in JavaScript
+// Answer: 
 
 // Todo 3.3 Declare an object with information about IT114L (course code, name, units, number of students)
 // Your code here
@@ -15,6 +24,9 @@
 
 // Todo 3.5 Calculate the total number of units you are taking this term using the array of objects.
 // Your code here
+
+// Checkpoint 3.2 What would be the equivalent of objects in python? How would you compare them?
+// Answer: 
 
 // Todo 3.6 Going back to the array of numbers, use the spread syntax to create a copy of the array with an additional number
 // Your code here
