ed41e77e9b0d4b1a8257aac60c0f65362893b4eb
jjlipat-mcl <154210736+jjlipat-mcl@users.noreply.github.com>
2024-01-12 21:51:32 +0000

    completed exercises 2 and 3

---
+++ 2. Control Structures/index.js
@@ -0,0 +1,36 @@
+const randomNumber = Math.floor((Math.random() * 100) + 1); // feel free to mock this value for testing
+
+// TODO 2.1 Display "more than fifty" if randomNumber is more than fify
+
+
+// TODO 2.2 Display whether the random number is odd or even
+
+
+// TODO 2.3 If the number if a multiple of 3, write "fizz".
+// if the number is a multiple of 5 display "buzz".
+// if the number is divisible by both 3 and 5, display "fizzbuzz". otherwise, display the number
+
+
+let toDisplay = ""
+// TODO 2.4 Use the ternary conditional operator to set toDisplay to "Even" if randomNumber is even and "Odd" if randomNumber is odd
+
+console.log("toDisplay", toDisplay);
+
+// TODO 2.5 Use a for loop to print the numbers 1 to N
+const n = 10
+
+
+// TODO 2.6 Use a for of loop to display all the values in the list
+const list = [""]
+
+
+// TODO 2.7 Use the for each method of the list to display all its values 
+
+
+// TODO 2.8 Use the try and catch block to catch division by zero errors in the code below.
+// In the finally block, simulate cleaning up resources by displaying "cleaning up resources"
+const numerator = Math.floor((Math.random() * 100) + 1);
+const denominator = Math.floor((Math.random() * 5)); // feel free to mock this value for testing
+
+const quotient = numerator / denominator;
+
---
+++ 3. Arrays and Objects/index.js
@@ -0,0 +1,23 @@
+// Todo 3.1 Declare an array of numbers
+
+
+// Todo 3.2 Get the first and last items in the array. calculate the average
+
+
+// Todo 3.3 Declare an object with information about IT114L (course code, name, units, number of students)
+
+
+// Todo 3.4 Add professor name as one of the fields of the object. Display the value of professor name.
+
+
+// Todo 3.5 Declare and array of objects with information about the courses you are taking this term
+
+
+// Todo 3.5 Calculate the total number of units you are taking this term using the array of objects.
+
+
+// Todo 3.6 Going back to the array of numbers, use the spread syntax to create a copy of the array with an additional number
+
+
+// Todo 3.7 Going back to your IT114L object, extract the course code and units using the spread operator
+
